a8aa5b4b6ee31742e43de4ba346fa7ade6b69872
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-11-03 13:52:40 +0100

    server update

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const sampleEmployee = {
+  name: {
+    first: "Charlie",
+    last: "Thompson",
+  },
+  email: "charlie.thompson@example.com",
+  picture: {
+    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+  },
+};
+
+const app = express();
+app.use(cors({ origin: ["http://localhost:3000"] }));
+
+app.get("/api/employees", (req, res) => {
+  res.json({ results: [sampleEmployee] });
+  });
+  const port = 3310;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
